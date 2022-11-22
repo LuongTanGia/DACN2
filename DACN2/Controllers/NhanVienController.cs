00dc580fd4baf75aac620ad8670559457e1ecead
Tan Gia Luong <tangia123@outlook.com.vn>
2022-11-22 18:20:28 +0700

    Nhan Vien Controller

---
+++ DACN2/Controllers/NhanVienController.cs
@@ -0,0 +1,31 @@
+﻿using DACN2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DACN2.Controllers
+{
+    public class NhanVienController : Controller
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        // GET: NhanVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListChang()
+        {
+            var sptl = from ss in data.Changs select ss;
+            return PartialView(sptl);
+
+        }
+        public ActionResult ListTour()
+        {
+            var sptl = from ss in data.Tours select ss;
+            return PartialView(sptl);
+
+        }
+    }
+}
